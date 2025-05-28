0fa4c89c7405de9ec5533ec30b603c4ada9ee562
tobbot16 <tobbot16@naver.com>
2025-05-28 21:50:27 +0900

    build.sh파일 수정

--- .render/build.sh
+++ .render/build.sh
@@ -1,14 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# 🟢 Java 설치
-apt-get update && apt-get install -y openjdk-17-jdk
-
-# 🟢 JAVA_HOME 설정
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+# Java 17 설치
+export JAVA_HOME=/usr/lib/jvm/java-17
 export PATH=$JAVA_HOME/bin:$PATH
 
-# 🟢 gradlew 실행 권한 부여 (혹시라도 안 되어있을 때를 대비)
-chmod +x ./gradlew
-
-# 🟢 빌드 실행 (테스트는 제외)
+# Gradle 빌드
+cd apiserver
 ./gradlew build -x test
