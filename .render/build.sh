4f829af55d93268b921dafe6679e738c59924882
tobbot16 <tobbot16@naver.com>
2025-05-28 22:00:11 +0900

    remove sudo

--- .render/build.sh
+++ .render/build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Java 설치
-sudo apt-get update
-sudo apt-get install -y openjdk-17-jdk
-
-# JAVA_HOME 설정
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+# JAVA_HOME (Render 환경에서는 보통 이미 Java 17/21이 설치돼 있음)
+export JAVA_HOME=$JAVA_HOME
 export PATH=$JAVA_HOME/bin:$PATH
 
 # 빌드
 cd apiserver
+chmod +x gradlew
 ./gradlew build -x test
