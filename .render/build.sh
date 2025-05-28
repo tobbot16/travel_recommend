d548f5746485fa4954ca89b87c6f40768010036c
tobbot16 <tobbot16@naver.com>
2025-05-28 21:54:11 +0900

    build.sh파일 수정

--- .render/build.sh
+++ .render/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Java 17 설치
-export JAVA_HOME=/usr/lib/jvm/java-17
+# Java 설치
+sudo apt-get update
+sudo apt-get install -y openjdk-17-jdk
+
+# JAVA_HOME 설정
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
-# Gradle 빌드
+# 빌드
 cd apiserver
 ./gradlew build -x test
